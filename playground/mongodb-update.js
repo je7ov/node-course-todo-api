7344dd3e70c4d7f365c870a00aec28f5a0d7dd84
Jeffrey Ovalles <jeffrey.ovalles@techclub.nyc>
2017-11-19 18:41:43 -0500

    Added mongoDB update playground

---
+++ playground/mongodb-update.js
@@ -0,0 +1,50 @@
+const { MongoClient, ObjectID } = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
+
+  // findOneAndUpdate()
+  // db.collection('Todos')
+  //   .findOneAndUpdate(
+  //     { 
+  //       text: 'Walk the dog'
+  //     },
+  //     {
+  //       $set: {
+  //         completed: false
+  //       }
+  //     },
+  //     {
+  //       returnOriginal: false
+  //     }
+  //   )
+  //   .then((result) => {
+  //     console.log(result);
+  //   })
+
+  db.collection('Users')
+    .findOneAndUpdate(
+      {
+        location: 'Boston'
+      },
+      {
+        $set: {
+          name: 'Tat'
+        },
+        $inc: {
+          age: 1
+        }
+      },
+      {
+        returnOriginal: false
+      }
+    )
+    .then((result) => {
+      console.log(result);
+    });
+
+  // db.close();
+}); 

2bda7c889932ce7df39aaf6fd0756587b36cd792
Jeffrey Ovalles <jeffrey.ovalles@techclub.nyc>
2017-11-20 01:32:10 -0500

    Hash user password before saving

--- server/models/user.js
+++ server/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const jwt = require('jsonwebtoken');
 const _ = require('lodash');
+const bcrypt = require('bcryptjs');
 
 const UserSchema = new mongoose.Schema({
   email: {
@@ -54,7 +55,7 @@ UserSchema.methods.toJSON = function () {
   const userObject = user.toObject();
 
   return _.pick(userObject, [ '_id', 'email' ]);
-}
+};
 
 UserSchema.statics.findByToken = function (token) {
   const User = this;
@@ -72,7 +73,22 @@ UserSchema.statics.findByToken = function (token) {
       'tokens.token': token,
       'tokens.access': 'auth'
     });
-}
+};
+
+UserSchema.pre('save', function (next) {
+  const user = this;
+  
+  if (user.isModified('password')) {
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(user.password, salt, (err, hash) => {
+        user.password = hash;
+        next();
+      });
+    });
+  } else { 
+    next();
+  }
+});
 
 const User = mongoose.model('User', UserSchema);
 
